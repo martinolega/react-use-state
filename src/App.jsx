71f199eb053d10befaba9ab331ce9463dd48bec8
martinolega <martinolega11@gmail.com>
2025-11-28 15:23:50 +0100

    feat:started

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-column align-center width-100">
+        <div className="container">
+          <h1>Learn Web Development</h1>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
